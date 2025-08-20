ac4b27cc7b06396823dbacb78045be85fa2332a3
misbah9304 <mmisbah2000@gmail.com>
2025-08-20 13:06:19 -0500

    Add files via upload

---
+++ TO-Do-List.js
@@ -0,0 +1,42 @@
+const inputbox = document.getElementById('searchbar');
+
+const listContainer = document.getElementById('list-container');
+
+function AddTask(){
+    if (inputbox.value === ''){
+        alert('you must write something!!');
+
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = inputbox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+
+    }
+     inputbox.value = '';
+    saveData();
+    }
+
+
+
+listContainer.addEventListener('click', function(e){
+    if (e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+    }
+    else if ( e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+
+}, false);
+
+function saveData(){
+    localStorage.setItem('Data', listContainer.innerHTML);
+}
+
+function getData(){
+    listContainer.innerHTML = localStorage.getItem('Data');
+}
+getData();
